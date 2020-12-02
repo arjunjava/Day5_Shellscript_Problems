42607b62ef43f783bcb50b00bd0aed3d2efb4681
arjunjava <jadhavarjun53@gmail.com>
2020-12-03 01:05:16 +0530

    1_5 Assignment created where we calculate inch into feet and 25 plot diemenssion

--- 01_5_unitConversion.sh
+++ 01_5_unitConversion.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -x
 
 echo -ne "1.Inch to Feet. \n2.Area of Rectangular Plot and Area of 25 Rectangular Plot. \nEnter The Your Choice: "
+
 read num
 
 case $num in
 1)
-	echo "Enter Number of Inches : "
-	read a
-	f2i=$(($a / 12))
-	echo "Number of Feet : $f2i ft"
+	read -p "Enter Number of Inches : " a
+	feet=$(($a / 12))
+	echo "Number of Feet : $feet ft"
 	;;
+2)
+        echo "Enter Width and Height (in feet) : "
+        read -p "enter width: " width
+	read -p "enter height: " height
+
+        react_area=$(($width * $height))
+        echo "Area of Rectangular plot is $react_area ft "
+	plot=$(( $react_area * 25 ))
+	echo "Area of 25 Plot of Dimension $width Feet * $height Feet is $plot ft "
+        ;;
+
+esac
